c8f289fe1e66d356365bde5bc11c36f083bfde3c
Pawan Bhatt <pawan.bhatt@ukg.com>
2024-02-20 21:16:42 +0530

    project checkin

---
+++ src/main/java/com/example/SpringReactiveExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveExamplesApplication.class, args);
+	}
+
+}
