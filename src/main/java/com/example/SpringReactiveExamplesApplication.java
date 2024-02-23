c5174b41bdaeb6cbaac11fa7ed055131e1be8fc8
Pawan Bhatt <pawan.bhatt@ukg.com>
2024-02-23 19:23:47 +0530

    Reactor operators examples and tests

--- src/main/java/com/example/SpringReactiveExamplesApplication.java
+++ src/main/java/com/example/SpringReactiveExamplesApplication.java
@@ -1,13 +1,133 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 public class SpringReactiveExamplesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringReactiveExamplesApplication.class, args);
+		SpringReactiveExamplesApplication app = new SpringReactiveExamplesApplication();
+
+		app.userNamesFlux().subscribe(name->System.out.println(name));
+		app.userNameMono().subscribe(name->System.out.println("Mono : Name " + name));
+		app.userNameCharMono().subscribe(output->System.out.println(output));
 	}
 
+	public Flux<String> transformExample(){
+
+		Function<Flux<String>, Flux<String>> toUpperAndLengthFunction = name ->
+				name.map(String::toUpperCase)
+						.filter(a->a.length() > 4);
+
+
+		return Flux.fromIterable(List.of("Pawan", "Anil"))
+				.transform(toUpperAndLengthFunction)
+				.flatMap(this::getChars)
+			//	.defaultIfEmpty("NULL")
+				.switchIfEmpty(Flux.just("NULL"));
+	}
+
+	public Flux<String> userNamesFlux(){
+		return Flux.fromIterable(List.of("Pawan", "Pmit", "Ayushi"))
+				.map(String::toUpperCase)
+				.filter(s->s.startsWith("P"))
+				.flatMap(s->getChars(s))
+				.concatMap(s->getChars(s))
+				.log();
+	}
+
+	public Flux<String> getChars(String a){
+		return Flux.fromArray(a.split(""));
+	}
+
+	public Mono<String> userNameMono(){
+		return Mono.just("Pawan")
+				.log();
+	}
+
+	public Mono<List<String>> userNameCharMono(){
+		return Mono.just("Pawan")
+				.flatMap(this::getCharsMono)
+				.log();
+	}
+
+	public Flux<String> userNameCharFlatMapMany(){
+		return Mono.just("Pawan")
+				.flatMapMany(this::getChars)
+				.log();
+	}
+
+	private Mono<List<String>> getCharsMono(String s) {
+
+		return Mono.just(List.of(s.split("")));
+
+	}
+
+	public Flux<String> concatExample(){
+
+		var flux1 = Flux.just("A");
+		var flux2 = Flux.just("B");
+
+		return Flux.concat(flux1, flux2);
+	}
+
+	public Flux<String> concatWithExample(){
+
+		var mono1 = Mono.just("A");
+		var flux1 = Flux.just("B");
+
+		return mono1.concatWith(flux1);
+	}
+
+	public Flux<String> mergeExample(){
+
+		var flux1 = Flux.just("A", "B")
+				.delayElements(Duration.ofMillis(100));
+		var flux2 = Flux.just("Y", "Z")
+				.delayElements(Duration.ofMillis(120));;
+
+		return Flux.merge(flux1, flux2).log();
+	}
+
+	public Flux<String> mergeWithExample(){
+
+		var mono1 = Mono.just("A")
+				.delayElement(Duration.ofMillis(100));
+		var flux2 = Flux.just("Y", "Z")
+				.delayElements(Duration.ofMillis(120));;
+
+		return mono1.mergeWith(flux2).log();
+	}
+
+	public Flux<String> mergeSequentialExample(){
+
+		var flux1 = Flux.just("A", "B")
+				.delayElements(Duration.ofMillis(100));
+		var flux2 = Flux.just("Y", "Z")
+				.delayElements(Duration.ofMillis(120));;
+
+		return Flux.mergeSequential(flux1, flux2).log();
+	}
+
+	public Flux<String> zipExample(){
+
+		var flux1 = Flux.just("A","B");
+		var flux2 = Flux.just("Y", "Z");;
+
+		return Flux.zip(flux1, flux2, (first, second)-> first + second).log();
+	}
+
+	public Flux<String> zipWithExample(){
+
+		var flux1 = Flux.just("A", "B");
+		var flux2 = Flux.just("Y", "Z");;
+
+		return flux1.zipWith(flux2, (f1, f2)->f1+f2).log();
+	}
 }
--- src/test/java/com/example/SpringReactiveExamplesApplicationTests.java
+++ src/test/java/com/example/SpringReactiveExamplesApplicationTests.java
@@ -1,13 +1,104 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
 class SpringReactiveExamplesApplicationTests {
 
+	SpringReactiveExamplesApplication app = new SpringReactiveExamplesApplication();
+
 	@Test
-	void contextLoads() {
+	void testFluxNames() {
+
+		var nameFlux = app.userNamesFlux();
+
+		StepVerifier.create(nameFlux)
+				.expectNext("P", "A", "W", "A", "N", "P", "M", "I", "T")
+				.verifyComplete();
 	}
 
+	@Test
+	void testUserNameCharMono(){
+
+		StepVerifier.create(app.userNameCharMono())
+				.expectNext(List.of("P","a","w","a","n"))
+				.verifyComplete();
+	}
+
+	@Test
+	void testUserNameCharFlatMapMany(){
+
+		StepVerifier.create(app.userNameCharFlatMapMany())
+				.expectNext("P","a","w","a","n")
+				.verifyComplete();
+
+	}
+
+	@Test
+	void testTransformExample(){
+
+		StepVerifier.create(app.transformExample())
+				//.expectNext("P","A","W","A","N")
+				.expectNext("NULL")
+				.verifyComplete();
+	}
+
+	@Test
+	void testConcatExample(){
+
+		StepVerifier.create(app.concatExample())
+				.expectNext("A", "B")
+				.verifyComplete();
+	}
+
+	@Test
+	void testConcatWithExample(){
+
+		StepVerifier.create(app.concatWithExample())
+				.expectNext("A", "B")
+				.verifyComplete();
+	}
+
+	@Test
+	void testMergeExample(){
+
+		StepVerifier.create(app.mergeExample())
+				.expectNext("A", "Y", "B", "Z")
+				.verifyComplete();
+	}
+
+	@Test
+	void testMergeWithExample(){
+
+		StepVerifier.create(app.mergeWithExample())
+				.expectNext("A", "Y", "Z")
+				.verifyComplete();
+	}
+
+	@Test
+	void testMergeSequentialExample(){
+
+		StepVerifier.create(app.mergeSequentialExample())
+				.expectNext("A", "B", "Y", "Z")
+				.verifyComplete();
+	}
+
+	@Test
+	void testZipExample(){
+
+		StepVerifier.create(app.zipExample())
+				.expectNext("AY", "BZ")
+				.verifyComplete();
+	}
+
+	@Test
+	void testZipWithExample(){
+
+		StepVerifier.create(app.zipWithExample())
+				.expectNext("AY", "BZ")
+				.verifyComplete();
+	}
 }
